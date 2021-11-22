0277bea6ed6d9f160b004b3266de78091084ea78
Mohanraj <mohanrajrengarajr@gmail.com>
2021-11-22 17:19:47 +0530

    Date and time picker

--- app/src/main/java/com/example/materialdesign/TextFields.kt
+++ app/src/main/java/com/example/materialdesign/TextFields.kt
@@ -5,19 +5,17 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
+import android.widget.TextView
+import com.google.android.material.datepicker.MaterialDatePicker
+import com.google.android.material.timepicker.MaterialTimePicker
+import com.google.android.material.timepicker.TimeFormat
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
 private const val ARG_PARAM1 = "param1"
 private const val ARG_PARAM2 = "param2"
 
-/**
- * A simple [Fragment] subclass.
- * Use the [TextFields.newInstance] factory method to
- * create an instance of this fragment.
- */
+
 class TextFields : Fragment() {
-    // TODO: Rename and change types of parameters
     private var param1: String? = null
     private var param2: String? = null
 
@@ -37,23 +35,29 @@ class TextFields : Fragment() {
         return inflater.inflate(R.layout.fragment_text_fields, container, false)
     }
 
-    companion object {
-        /**
-         * Use this factory method to create a new instance of
-         * this fragment using the provided parameters.
-         *
-         * @param param1 Parameter 1.
-         * @param param2 Parameter 2.
-         * @return A new instance of fragment TextFields.
-         */
-        // TODO: Rename and change types and number of parameters
-        @JvmStatic
-        fun newInstance(param1: String, param2: String) =
-            TextFields().apply {
-                arguments = Bundle().apply {
-                    putString(ARG_PARAM1, param1)
-                    putString(ARG_PARAM2, param2)
-                }
-            }
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val context = context as MainActivity
+        val date = context.findViewById<Button>(R.id.pick_date_button)
+        val time = context.findViewById<Button>(R.id.pick_time_button)
+        val builder =     MaterialDatePicker.Builder.datePicker()
+            .setTitleText("Select date")
+            .setSelection(MaterialDatePicker.todayInUtcMilliseconds())
+            .build()
+        val picker =
+            MaterialTimePicker.Builder()
+                .setTimeFormat(TimeFormat.CLOCK_12H)
+                .setHour(12)
+                .setMinute(10)
+                .setTitleText("Select Appointment time")
+                .build()
+
+        date.setOnClickListener{
+            fragmentManager?.let { it1 -> builder.show(it1,"d") }
+        }
+        time.setOnClickListener{
+            fragmentManager?.let { it1 -> picker.show(it1,"d") }
+        }
+
     }
 }
